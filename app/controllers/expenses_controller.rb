fbc0d5f229090404b76c03329c1b41c815d1c86e
anderson <anderson@debian.anderson.com>
2017-12-28 10:47:40 -0500

    Implementa la solución a Gastos por Usuario

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,14 +1,19 @@
 class ExpensesController < ApplicationController
   def index
 
-  	 @expenses = Expense.order("date DESC")
-  	 
-  	 if params[:concept].present?
-  	 	@expenses = @expenses.where("concept like ?", '%' + params[:concept] + '%')
-  	 end
+  	 if user_signed_in?
 
-  	 if params[:category_id].present?
-  	 	@expenses = @expenses.where("category_id = ?", params[:category_id])
-  	 end
+	  	 @expenses = Expense.order("date DESC").where("user_id = ?", current_user)
+	  	 
+	  	 if params[:concept].present?
+	  	 	@expenses = @expenses.where("concept like ?", '%' + params[:concept] + '%')
+	  	 end
+
+	  	 if params[:category_id].present?
+	  	 	@expenses = @expenses.where("category_id = ?", params[:category_id])
+	  	 end
+	  else
+	  	redirect_to user_session_path
+	  end
   end
 end
