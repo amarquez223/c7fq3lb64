a013fe6b9c86b7ffb38cdfd4fdb753f92321e52b
anderson <anderson@debian.anderson.com>
2017-12-27 19:52:20 -0500

    Implementación de Filtrando Gastos

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,15 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+  	 cri_concepto = params[:concept]
+  	 if cri_concepto.nil?
+  	 	cri_concepto = ''
+  	 end
+
+  	 cri_categoria = params[:category_id]
+	 if cri_categoria.nil?
+  	 	cri_categoria = ''
+  	 end
+
+  	 @expenses = Expense.where("concept like ? AND category_id like ?", '%' + cri_concepto + '%', '%' + cri_categoria + '%').order("date DESC")
   end
 end
