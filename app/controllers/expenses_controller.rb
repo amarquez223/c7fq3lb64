987cbe68b9c35700e46b0bfeb5542a0e415a93c1
anderson <anderson@debian.anderson.com>
2017-12-28 11:30:55 -0500

    Ajuste de authenticate_user!

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,19 +1,17 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
 
-  	 if user_signed_in?
+  	 @expenses = Expense.order("date DESC").where("user_id = ?", current_user)
+  	 
+  	 if params[:concept].present?
+  	 	@expenses = @expenses.where("concept like ?", '%' + params[:concept] + '%')
+  	 end
 
-	  	 @expenses = Expense.order("date DESC").where("user_id = ?", current_user)
-	  	 
-	  	 if params[:concept].present?
-	  	 	@expenses = @expenses.where("concept like ?", '%' + params[:concept] + '%')
-	  	 end
-
-	  	 if params[:category_id].present?
-	  	 	@expenses = @expenses.where("category_id = ?", params[:category_id])
-	  	 end
-	  else
-	  	redirect_to user_session_path
-	  end
+  	 if params[:category_id].present?
+  	 	@expenses = @expenses.where("category_id = ?", params[:category_id])
+  	 end
   end
+
 end
